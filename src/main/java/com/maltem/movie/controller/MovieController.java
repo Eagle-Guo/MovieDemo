2708889b78d099030f44ca4eb0f55513ee55c20f
HuanPing Guo <guo.huanping@mirumagency.com>
2018-06-13 00:27:13 +0800

    Add the add/update/delete API for restful

--- src/main/java/com/maltem/movie/controller/MovieController.java
+++ src/main/java/com/maltem/movie/controller/MovieController.java
@@ -1,18 +1,24 @@
 package com.maltem.movie.controller;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.maltem.movie.entity.Movie;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,18 +26,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
-	 /**
-	  * list all the movie data
-	  * @return
-	  */
-	 @RequestMapping(value="list", method=RequestMethod.GET, consumes="application/json") 
-	 public List<Movie> listAll (){
-		 ObjectMapper mapper = new ObjectMapper();
-		 List<Movie> movies = new ArrayList<Movie>();
+	
+	Logger logger = LoggerFactory.getLogger(MovieController.class);
+	
+	/**
+	 * load the data from movies.json file
+	 */
+	public List<Movie> loadJsonFromFile() {
+		ObjectMapper mapper = new ObjectMapper();
+		List<Movie> movies = new ArrayList<Movie>();
 		try {
+			logger.info("start loading the json file");
 			TypeReference<List<Movie>> mapType = new TypeReference<List<Movie>>() {};
 			InputStream is = TypeReference.class.getResourceAsStream("/static/movies.json");
 			movies = mapper.readValue(is, mapType);
+			logger.info("Json size : {}" ,movies.size());
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
@@ -39,7 +48,16 @@ public class MovieController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		 
+		return movies;
+	}
+	 /**
+	  * list all the movie data
+	  * @return
+	  */
+	 @RequestMapping(value="list", method=RequestMethod.GET, consumes="application/json") 
+	 public List<Movie> listAll (){
+		 List<Movie> movies = loadJsonFromFile();
+		 logger.info("List all the movies");
 		 return movies;
 	 }
 	 
@@ -48,18 +66,128 @@ public class MovieController {
 	  * @param name
 	  * @return
 	  */
-	 @RequestMapping(value="get/{name}", method=RequestMethod.GET, consumes="application/json") 
-	 public Movie getMovieByName (@PathVariable String name){
-		 Movie movie = new Movie();
-		 movie.setTitle("Ocean's 8");
-		 movie.setDirector("Gary Ross");
-		 Calendar cal = Calendar.getInstance();
-		 cal.set(2018, 06, 13);
-		 movie.setReleaseDate(cal.getTime());
-		 movie.setType("action");
-		 //String jsonInString = mapper.writeValueAsString(obj);
+	 @RequestMapping(value="{title}", method=RequestMethod.GET, consumes="application/json") 
+	 public Movie getMovieByTitle (@PathVariable String title){
+		 logger.info("Get the movie with title {}", title);
+		 if (title == null || "".equals(title)) {
+			 return null;
+		 }
+		 
+		 List<Movie> movies = loadJsonFromFile();
+		 
+		 for (Movie movie : movies) {
+			 if (title.equals(movie.getTitle())) {
+				 return movie;
+			 }
+		 }
+		 return null;
+	 }
+	 
+	 /**
+	  * Delete the movie base on title
+	  * @param title
+	  * @return
+	  */
+	 @RequestMapping(value="{title}", method=RequestMethod.DELETE, consumes="application/json") 
+	 public Movie deleteMovieByTitle (@PathVariable String title){
+		 logger.info("Delete the movie with title {}", title);
+		 if (title == null || "".equals(title)) {
+			 return null;
+		 }
+		 
+		 List<Movie> movies = loadJsonFromFile();
+		 Movie removeMovie = null;
+		 for (Movie movie : movies) {
+			 if (title.equals(movie.getTitle())) {
+				 removeMovie = movie;
+			 }
+		 }
+		 if (removeMovie != null) {
+			 movies.remove(removeMovie);
+
+			 ObjectMapper mapper = new ObjectMapper();
+			 ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+			 
+			 try {
+				writer.writeValue(new File("C:\\huanping\\MovieDemo\\src\\main\\resources\\static\\movies.json"), movies);
+			 } catch (JsonGenerationException e1) {
+				e1.printStackTrace();
+			 } catch (JsonMappingException e1) {
+				e1.printStackTrace();
+			 } catch (IOException e1) {
+				e1.printStackTrace();
+			 }
+		 }
+		
+		 return removeMovie;
+	 }
+	 
+	 /**
+	  * Add the new movie into list
+	  * @param movie
+	  * @return
+	  */
+	 @RequestMapping(method=RequestMethod.POST, consumes="application/json") 
+	 public Movie addMovie (@RequestBody Movie movie){
+		 logger.info("Add the movie {}", movie);
+		 if (movie == null) {
+			 return null;
+		 }
+		 
+		 List<Movie> movies = loadJsonFromFile();
+		 for (Movie submovie : movies) {
+			 if (movie.equals(submovie)) {
+				 return movie;
+			 }
+		 }
+		 movies.add(movie);
+		 ObjectMapper mapper = new ObjectMapper();
+		 ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+		 try {
+			writer.writeValue(new File("C:\\huanping\\MovieDemo\\src\\main\\resources\\static\\movies.json"), movies);
+		 } catch (JsonGenerationException e1) {
+			e1.printStackTrace();
+		 } catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		 } catch (IOException e1) {
+			e1.printStackTrace();
+		 }
 		 return movie;
 	 }
 	 
+	 /**
+	  * Update the movie
+	  * @param movie
+	  * @return
+	  */
+	 @RequestMapping(method=RequestMethod.PUT, consumes="application/json") 
+	 public Movie updateMovie (@RequestBody Movie movie){
+		 logger.info("Add the movie {}", movie);
+		 if (movie == null) {
+			 return null;
+		 }
+		 
+		 List<Movie> movies = loadJsonFromFile();
+		 Movie removeMovie = null;
+		 for (Movie submovie : movies) {
+			 if (movie.getTitle().equals(submovie.getTitle())) {
+				 removeMovie = submovie;
+			 }
+		 }
+		 movies.remove(removeMovie);
+		 movies.add(movie);
+		 ObjectMapper mapper = new ObjectMapper();
+		 ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+		 try {
+			writer.writeValue(new File("C:\\huanping\\MovieDemo\\src\\main\\resources\\static\\movies.json"), movies);
+		 } catch (JsonGenerationException e1) {
+			e1.printStackTrace();
+		 } catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		 } catch (IOException e1) {
+			e1.printStackTrace();
+		 }
+		 return movie;
+	 }
 	 
 }
--- src/main/java/com/maltem/movie/entity/Movie.java
+++ src/main/java/com/maltem/movie/entity/Movie.java
@@ -48,5 +48,56 @@ public class Movie {
 	public void setType(String type) {
 		this.type = type;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((director == null) ? 0 : director.hashCode());
+		result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (director == null) {
+			if (other.director != null)
+				return false;
+		} else if (!director.equals(other.director))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [title=" + title + ", director=" + director + ", releaseDate=" + releaseDate + ", type=" + type
+				+ "]";
+	}
+	
+	
 	
 }
